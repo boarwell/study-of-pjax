ae2dae19c3539113501c8820dbd5cfa8fd70cd49
boarwell <34891695+boarwell@users.noreply.github.com>
2019-12-11 00:46:10 +0900

    implement body contents replacement

--- build/main.js
+++ build/main.js
@@ -1,21 +1,42 @@
 "use strict";
 const getHTML = async (src) => {
-    const res = await fetch(src);
+    // if we don't specify { cache: 'no-store' },
+    // Chrome caches responses and doesn't send request (on my environment).
+    const res = await fetch(src, { cache: 'no-store' });
     const html = await res.text();
     const domParser = new DOMParser();
     const fetchedDOM = domParser.parseFromString(html, 'text/html');
     return {
-        head: fetchedDOM.head
+        head: fetchedDOM.head,
+        root: fetchedDOM.querySelector('#domrep-root')
     };
 };
 const replaceHead = (newDOM) => {
     document.head.replaceWith(newDOM.head);
 };
+const replaceRoot = (newDOM) => {
+    const root = document.querySelector('#domrep-root');
+    root.replaceWith(newDOM.root);
+};
+const replaceDOM = (newDOM) => {
+    replaceHead(newDOM);
+    replaceRoot(newDOM);
+};
 const main = () => {
-    const button = document.querySelector('#sample');
-    button.addEventListener('click', async () => {
+    const sampleButton = document.querySelector('#sample');
+    sampleButton.addEventListener('click', async () => {
         const res = await getHTML('/sample.html');
-        replaceHead(res);
+        replaceDOM(res);
+        // TODO: add DOM string as data so that
+        // we can restore page contents when we pushed the back button.
+        history.pushState(null, '', '/sample.html');
+    });
+    const indexButton = document.querySelector('#index');
+    indexButton.addEventListener('click', async () => {
+        const res = await getHTML('/index.html');
+        replaceDOM(res);
+        // TODO: same as above.
+        history.pushState(null, '', '/index.html');
     });
 };
 main();
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 type FetchedDOM = {
   head: HTMLHeadElement;
+  root: HTMLElement;
 };
 
 const getHTML = async (src: string): Promise<FetchedDOM> => {
-  const res = await fetch(src);
+  // if we don't specify { cache: 'no-store' },
+  // Chrome caches responses and doesn't send request (on my environment).
+  const res = await fetch(src, { cache: 'no-store' });
   const html = await res.text();
   const domParser = new DOMParser();
   const fetchedDOM = domParser.parseFromString(html, 'text/html');
 
   return {
-    head: fetchedDOM.head
+    head: fetchedDOM.head,
+    root: fetchedDOM.querySelector('#domrep-root') as HTMLElement
   };
 };
 
@@ -17,11 +21,32 @@ const replaceHead = (newDOM: FetchedDOM): void => {
   document.head.replaceWith(newDOM.head);
 };
 
+const replaceRoot = (newDOM: FetchedDOM): void => {
+  const root = document.querySelector('#domrep-root') as HTMLElement;
+  root.replaceWith(newDOM.root);
+};
+
+const replaceDOM = (newDOM: FetchedDOM): void => {
+  replaceHead(newDOM);
+  replaceRoot(newDOM);
+};
+
 const main = () => {
-  const button = document.querySelector('#sample')!;
-  button.addEventListener('click', async () => {
+  const sampleButton = document.querySelector('#sample')!;
+  sampleButton.addEventListener('click', async () => {
     const res = await getHTML('/sample.html');
-    replaceHead(res);
+    replaceDOM(res);
+    // TODO: add DOM string as data so that
+    // we can restore page contents when we pushed the back button.
+    history.pushState(null, '', '/sample.html');
+  });
+
+  const indexButton = document.querySelector('#index')!;
+  indexButton.addEventListener('click', async () => {
+    const res = await getHTML('/index.html');
+    replaceDOM(res);
+    // TODO: same as above.
+    history.pushState(null, '', '/index.html');
   });
 };
 
