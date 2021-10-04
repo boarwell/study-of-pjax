9385ceec17dbb3ab1c3664f428785ddd40f74e3e
boarwell <34891695+boarwell@users.noreply.github.com>
2021-10-05 02:31:10 +0900

    npm -> pnpm, pnpm update --latest

--- build/main.js
+++ build/main.js
@@ -1,18 +1,26 @@
 "use strict";
 const ID = 'domrep-root';
+/**
+ * convert dom elements to string
+ */
 const serialize = ({ head, root }) => {
     const headHTML = head.outerHTML;
     const rootHTML = root.outerHTML;
     return JSON.stringify({ head: headHTML, root: rootHTML });
 };
+/**
+ * full (fetched) html to dom elements
+ */
 const createDOMFrom = (html) => {
-    const dp = new DOMParser();
-    const dom = dp.parseFromString(html, 'text/html');
+    const dom = new DOMParser().parseFromString(html, 'text/html');
     return {
         head: dom.head,
         root: dom.querySelector(`#${ID}`)
     };
 };
+/**
+ * rewrite current page by the argument
+ */
 const restorePage = (serializedDOMString) => {
     const state = JSON.parse(serializedDOMString);
     const head = document.createElement('head');
@@ -22,6 +30,9 @@ const restorePage = (serializedDOMString) => {
     const dom = { head, root };
     replaceDOM(dom);
 };
+/**
+ * fetch html from the src and create dom element
+ */
 const getHTML = async (src) => {
     // if we don't specify { cache: 'no-store' },
     // Chrome caches responses and doesn't send request (on my environment).
@@ -29,9 +40,15 @@ const getHTML = async (src) => {
     const html = await res.text();
     return createDOMFrom(html);
 };
+/**
+ * replace head element
+ */
 const replaceHead = (newDOM) => {
     document.head.replaceWith(newDOM.head);
 };
+/**
+ * replace root element
+ */
 const replaceRoot = (newDOM) => {
     const root = document.querySelector(`#${ID}`);
     // if we use `replaceWith()` to the observed element,
@@ -39,6 +56,9 @@ const replaceRoot = (newDOM) => {
     root.innerHTML = '';
     root.appendChild(newDOM.root);
 };
+/**
+ * (currently) replace head and root element
+ */
 const replaceDOM = (newDOM) => {
     replaceHead(newDOM);
     replaceRoot(newDOM);
@@ -54,9 +74,13 @@ const saveCurrentState = () => {
     const html = document.documentElement.outerHTML;
     history.replaceState(serialize(createDOMFrom(html)), '', null);
 };
-const hasHREF = (a) => a.href !== '';
-const withoutHash = (a) => new URL(a.href).hash === '';
-const isSameOriginWith = (currentURL) => (a) => new URL(a.href).origin === currentURL.origin;
+const hasHREF = ({ href }) => href !== '';
+const withoutHash = ({ href }) => new URL(href).hash === '';
+const isSameOriginWith = (currentURL) => ({ href }) => new URL(href).origin === currentURL.origin;
+/**
+ * set an eventListener for <a> tags:
+ * fetch the target HTML, replace DOM, and pushState
+ */
 const setup = (root) => {
     const currentURL = new URL(location.href);
     const listOfATag = [...root.querySelectorAll('a')];
@@ -74,9 +98,16 @@ const setup = (root) => {
         });
     });
 };
+/**
+ * restore page content
+ */
 const onPopstate = (ev) => {
     restorePage(ev.state);
 };
+/**
+ * when the mutation type is 'childList',
+ * get the root element and setup
+ */
 const onMutation = (mutations) => {
     console.log('mutation');
     mutations
@@ -88,6 +119,9 @@ const onMutation = (mutations) => {
         }
     });
 };
+/**
+ * main
+ */
 const main = () => {
     saveCurrentState();
     const root = document.querySelector(`#${ID}`);
